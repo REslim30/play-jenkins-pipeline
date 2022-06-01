30f98a7d191d7029cbd1d99c02a034d80f2fa0eb
Aidan Do <aidan.do@nqry.com.au>
2022-06-01 13:57:54 +0930

    steps

--- src/org/foo/HelloWorld.groovy
+++ src/org/foo/HelloWorld.groovy
@@ -1,6 +1,11 @@
 package org.foo
 
 class HelloWorld implements Serializable {
+    def steps
+    HelloWorld(steps) {
+        this.steps = steps
+    }
+
     def hello() {
         script.echo("hello")
     }
