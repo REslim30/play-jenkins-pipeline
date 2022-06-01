48840463431c6839cf28b5c46bae7eb6bf7dc53f
Aidan Do <aidan.do@nqry.com.au>
2022-06-01 15:26:17 +0930

    properties

--- src/org/foo/HelloWorld.groovy
+++ src/org/foo/HelloWorld.groovy
@@ -8,5 +8,13 @@ class HelloWorld implements Serializable {
 
     def hello() {
         script.echo("hello from feature/test")
+
+        script.echo("Testing multiple calls to script.properties...")
+        script.properties([
+            script.buildDiscarder(script.logRotator(numToKeepStr: '5'))
+        ])
+
+        script.properties([
+        ])
     }
 }
